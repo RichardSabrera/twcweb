93fea87e8903e968bc43ca81e14aae3ca6d48f8c
Richard <richard@Richards-MacBook-Air.local>
2023-10-25 18:21:14 -0500

    cambio

--- axios.js
+++ axios.js
@@ -1,68 +1,107 @@
 
 //OBTENER NOSOTROS
-axios.get('https://appapiwebtwc.azurewebsites.net/api/SiteWeb/Us')
-    .then((respuesta) => {
+$('#loader').show();
 
-        $("#usText").text(respuesta.data[0].description);
+all();
 
-    }).catch(() => {
+$('#loader').hide();
 
-        console.log('No funcionas')
-    })
+function all() {
+    $.ajax({
+        url: 'https://zealous-bay-083bf620f.4.azurestaticapps.net/api/SiteWeb/GetCargarTodo',
+        method: 'GET',
+        success: function (respuesta) {
 
-axios.get('https://appapiwebtwc.azurewebsites.net/api/SiteWeb/Purpose')
-    .then((respuesta) => {
+            $("#usText").text(respuesta.appUs[0].description);
+            $("#idPurpose").text(respuesta.appPurpose[0].description);
 
-        $("#idPurpose").text(respuesta.data[0].description);
+            var carouselInner = $(".carousel-inner");
+            var carouselIndicators = $(".carousel-indicators");
 
-    }).catch(() => {
+            respuesta.appbanner.forEach(function (item, index) {
+                var carouselItem = $("<div>").addClass("carousel-item");
+                if (index === 0) {
+                    carouselItem.addClass("active");
+                }
+                var image = $("<img>").addClass("d-block w-100").attr("src", "data:image/png;base64," + item.image);
+                carouselItem.append(image);
+                carouselInner.append(carouselItem);
 
-        console.log('No funcionas')
-    })
+                var indicator = $("<button>")
+                    .attr("type", "button")
+                    .attr("data-bs-target", "#carouselExampleIndicators")
+                    .attr("data-bs-slide-to", index)
+                    .attr("aria-label", "Slide " + (index + 1));
+                if (index === 0) {
+                    indicator.addClass("active");
+                }
+                carouselIndicators.append(indicator);
+            });
 
-//OBTENER BANNER
-axios.get('https://appapiwebtwc.azurewebsites.net/API/SiteWeb/BANNER')
-    .then((respuesta) => {
+        },
+        error: function () {
+            console.log('No funcionas');
+        }
+    });
+}
 
-        //GENERAMOS CARRUSEL
-        var carouselInner = $(".carousel-inner");
-        var carouselIndicators = $(".carousel-indicators");
 
-        // Itera sobre los datos de la API y genera los elementos del carousel
-        respuesta.data.forEach(function (item, index) {
-            // Crea el elemento del carousel
-            var carouselItem = $("<div>").addClass("carousel-item");
+function US() {
+    $.ajax({
+        url: 'https://localhost:7167/api/SiteWeb/Us',
+        method: 'GET',
+        success: function (respuesta) {
+            $("#usText").text(respuesta[0].description);
+        },
+        error: function () {
+            console.log('No funcionas');
+        }
+    });
+}
 
-            // Si es el primer elemento, añade la clase 'active'
-            if (index === 0) {
-                carouselItem.addClass("active");
-            }
+function Purpose() {
+    $.ajax({
+        url: 'https://localhost:7167/api/SiteWeb/Purpose',
+        method: 'GET',
+        success: function (respuesta) {
+            $("#idPurpose").text(respuesta[0].description);
+        },
+        error: function () {
+            console.log('No funcionas');
+        }
+    });
+}
 
-            // Crea la imagen y establece el atributo 'src' con el valor de 'image' de la API
-            var image = $("<img>").addClass("d-block w-100").attr("src", "data:image/png;base64," + item.image);
+function BANNER() {
+    $.ajax({
+        url: 'https://localhost:7167/api/SiteWeb/BANNER',
+        method: 'GET',
+        success: function (respuesta) {
+            var carouselInner = $(".carousel-inner");
+            var carouselIndicators = $(".carousel-indicators");
 
-            // Agrega la imagen al elemento del carousel
-            carouselItem.append(image);
+            respuesta.forEach(function (item, index) {
+                var carouselItem = $("<div>").addClass("carousel-item");
+                if (index === 0) {
+                    carouselItem.addClass("active");
+                }
+                var image = $("<img>").addClass("d-block w-100").attr("src", "data:image/png;base64," + item.image);
+                carouselItem.append(image);
+                carouselInner.append(carouselItem);
 
-            // Agrega el elemento del carousel al elemento 'carouselInner'
-            carouselInner.append(carouselItem);
-
-            // Crea el indicador del carousel
-            var indicator = $("<button>")
-                .attr("type", "button")
-                .attr("data-bs-target", "#carouselExampleIndicators")
-                .attr("data-bs-slide-to", index)
-                .attr("aria-label", "Slide " + (index + 1));
-
-            // Si es el primer indicador, añade la clase 'active'
-            if (index === 0) {
-                indicator.addClass("active");
-            }
-
-            // Agrega el indicador al elemento 'carouselIndicators'
-            carouselIndicators.append(indicator);
-        });
-    }).catch(() => {
-
-        console.log('No funcionas')
-    })
+                var indicator = $("<button>")
+                    .attr("type", "button")
+                    .attr("data-bs-target", "#carouselExampleIndicators")
+                    .attr("data-bs-slide-to", index)
+                    .attr("aria-label", "Slide " + (index + 1));
+                if (index === 0) {
+                    indicator.addClass("active");
+                }
+                carouselIndicators.append(indicator);
+            });
+        },
+        error: function () {
+            console.log('No funcionas');
+        }
+    });
+}
